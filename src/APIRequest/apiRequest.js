fc9cf31f2e63377767d8b5f6b15e4cdc4f7d80ed
MD.Rakib <kamrulhasanrakib99@gmail.com>
2024-03-24 21:41:12 +0600

    Change Base url

--- src/APIRequest/apiRequest.js
+++ src/APIRequest/apiRequest.js
@@ -17,7 +17,7 @@ import {
 } from "../Redux/StateSlice/task-slice";
 import { setSummary } from "../Redux/StateSlice/summary-slice";
 import { setProfile } from "../Redux/StateSlice/profile-slice";
-const BaseURL = "http://localhost:3010/api/v1";
+const BaseURL = "https://task-manager-backend-one.vercel.app/api/v1";
 
 const axiosHeader = { headers: { token: getToken() } };
 
